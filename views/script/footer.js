8f9347e67bf6c5139c13bca64ba35ba34078f174
T0mmy <antchupinin@gmail.com>
2023-01-13 00:43:15 +0300

    login and register add

---
+++ views/script/footer.js
@@ -0,0 +1,12 @@
+// const windowInnerHeight = document.documentElement.clientHeight
+// const pageHeight = document.body.clientHeight;
+// console.log(windowInnerHeight)
+// console.log(pageHeight)
+// if (windowInnerHeight > pageHeight){
+//     const footer = document.querySelector("footer");
+//     console.log(footer)
+
+//     footer.style.position = "absolute";
+//     footer.style.right = "0";
+//     footer.style.bottom = "0";
+// }
