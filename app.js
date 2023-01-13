9e7ec4543503d8454c5b4049c819b1a17c1fe285
T0mmy <antchupinin@gmail.com>
2023-01-14 01:45:36 +0300

    html replace to ejs

--- app.js
+++ app.js
@@ -6,12 +6,26 @@ const PORT = 4000;
 
 const app = express();
 app.use(express.json());
-app.use(express.static('views'));
+app.use(express.static(__dirname + '/views'));
+app.set('view engine', 'ejs');
 
 const startUp = () => {
 
     app.listen(PORT, () => console.log(`server start on ${PORT} port`));
+    // console.log(__dirname)
 }
+
 
 startUp();
 
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.get('/login', (req, res) =>{
+    res.render('login');
+})
+
+app.get('/register', (req, res) =>{
+    res.render('register');
+})
