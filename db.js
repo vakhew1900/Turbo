4eb508414d1667d2c5639de1cb61667c3af3275d
T0mmy <antchupinin@gmail.com>
2023-02-02 02:15:37 +0300

    add one-many and one-one

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const {DataTypes, Sequelize} = require('sequelize');
+const { DataTypes, Sequelize } = require('sequelize');
 const configDB = require('./model/configDB')
 
 const sequelize = new Sequelize(
@@ -21,6 +21,104 @@ const Draft = require('./model/Draft')(sequelize);
 const News = require('./model/News')(sequelize);
 const Comment = require('./model/Comment')(sequelize);
 
+// Type-MultiContent
+
+Type.hasMany(MultiContent,
+    {
+        foreignKey: 'type_id'
+    }
+);
+
+MultiContent.belongsTo(Type,
+    {
+        foreignKey: 'type_id'
+    }
+);
+
+
+// Status - User
+
+Status.hasMany(User,
+    {
+        foreignKey: "status_id"
+    }
+)
+
+User.belongsTo(Status,
+    {
+        foreignKey: "status_id"
+    }
+)
+
+// Page-User
+User.hasMany(Page,
+    {
+        foreignKey: "author_id"
+    }
+)
+
+Page.belongsTo(User,
+    {
+        foreignKey: "author_id"
+    }
+)
+
+
+// MultiContent User
+MultiContent.hasOne( User,
+    {
+        foreignKey: "avatar_id"
+    }
+);
+
+User.belongsTo(MultiContent,
+    {
+        foreignKey: "avatar_id"
+    }
+);
+
+// Draft- Page
+Page.hasOne(Draft,
+    {
+        foreignKey: "draft_id"
+    }
+);
+
+Draft.belongsTo(Page,
+    {
+        foreignKey: "draft_id"
+    }
+);
+
+// News - Page
+
+Page.hasOne(News,
+    {
+        foreignKey: "news_id"
+    }
+);
+
+News.belongsTo(Page,
+    {
+        foreignKey: "news_id"
+    }
+);
+
+// Comment-MultiContent
+MultiContent.hasOne( Comment,
+    {
+        foreignKey: "multi_content_id"
+    }
+);
+
+Comment.belongsTo(MultiContent,
+    {
+        foreignKey: "multi_content_id"
+    }
+);
+
+
+
 // const type = Type.create({name: "active"});
 // const multi_content = MultiContent.create({path: "./images", name: "image", type_id : 1 })
 // const status = Status.create({name : "active"});
@@ -29,3 +127,39 @@ const Comment = require('./model/Comment')(sequelize);
 // const draft = Draft.create({draft_id: 2});
 // const news = News.create({news_id: 2, title :"title", subtitle : "subtitle", main_image_id : 1 });
 // const comment = Comment.create({text_content: "2222", multi_content_id : 1});
+
+// MultiContent.findAll(
+//     {
+//         include : Type
+//     }
+// ).then(res => console.log(JSON.stringify(res, null, 2)));
+
+// User.findAll(
+//     {
+//         include: [Status, MultiContent]
+//     }
+// ).then(res => console.log(JSON.stringify(res, null, 2)));
+
+// Page.findAll(
+//     {
+//         include: User
+//     }
+// ).then(res => console.log(JSON.stringify(res, null, 2)));
+
+// Draft.findAll(
+//     {
+//         include: Page
+//     }
+// ).then(res => console.log(JSON.stringify(res, null, 2)));
+
+// News.findAll(
+//     {
+//         include: Page
+//     }
+// ).then(res => console.log(JSON.stringify(res, null, 2)));
+
+// Comment.findAll(
+//     {
+//         include: MultiContent
+//     }
+// ).then(res => console.log(JSON.stringify(res, null, 2)));
