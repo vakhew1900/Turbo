a363a2dcf822864a7af09c3e9b6a1f90ae3b3d17
T0mmy <antchupinin@gmail.com>
2023-03-21 23:20:04 +0300

    add CommentService

---
+++ service/CommentService.js
@@ -0,0 +1,25 @@
+const {Comment, Content} = require('../db')
+
+
+class CommentService{
+
+    async create(news_id, text_content, multi_content){
+        
+        if(news_id == null || typeof news_id != 'number'){
+            throw new Error('it`s not a comment for news');
+        }
+
+        
+        let content_id = null;
+
+        if (multi_content != null){
+            const content = await Content.create({path : multiContent.path, name: multiContent.name, type_id: type.type_id});
+            content_id = content.content_id;
+        }
+        
+        const comment = await Comment.create({news_id : news_id, text_content: text_content, content_id : content_id});
+
+        return comment;
+    }
+}
+
